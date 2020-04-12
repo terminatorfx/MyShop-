a7733b7ea37a3d9b3e917a7388274850f7989338
Kaleemullah <Kaleemu-khan@hotmail.com>
2020-04-12 22:08:40 +0400

    done

---
+++ MyShop/MyShop.Core/Models/BasketItems.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+   public class BasketItems:BaseEntity
+    {
+        public string BasketID { get; set; }
+        public string ProductID { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
