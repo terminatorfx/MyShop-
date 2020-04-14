7e00cf03154e73e62a4b1aef050e794ccda5edcf
Kaleemullah <Kaleemu-khan@hotmail.com>
2020-04-14 22:30:38 +0400

    done.

---
+++ MyShop/MyShop.WebUI/Controllers/BasketController.cs
@@ -0,0 +1,25 @@
+﻿using MyShop.Core.Contracts;
+using MyShop.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyShop.WebUI.Controllers
+{
+    public class BasketController : Controller
+    {
+        IBasketService basketService;
+
+        public BasketController(IBasketService BasketService)
+        {
+            this.basketService = BasketService;
+        }
+        // GET: Basket
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
